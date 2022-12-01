b084fdfc4e2926cb580509f06006e1346a366831
Franco Arendholz <franco.arendholz@agile-rcm.de>
2022-12-01 16:41:54 +0100

    Run pipeline scripts on Jenkins on the fly

--- manage/groovy/systemMessage.groovy
+++ manage/groovy/systemMessage.groovy
@@ -1,4 +1,14 @@
+{% autoescape off %}
 message='{{ message|capfirst }}'
 Jenkins jenkins = Jenkins.get()
-jenkins.setSystemMessage(message)
-jenkins.save()
+
+try {
+    jenkins.setSystemMessage(message)
+    jenkins.save()
+} catch(Throwable err) {
+    println('There was an error setting the new system message!\n'+err.toString)
+    throw err
+}
+
+println('Successfully set new System Message!')
+{% endautoescape %}
